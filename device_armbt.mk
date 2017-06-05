c372391b875aa9e11995e61a05d32fabb53d307b
Andrey Rogov <rogovandrey@eltechs.com>
2017-06-05 20:39:24 +0300

    Attempt 6

--- device_armbt.mk
+++ device_armbt.mk
@@ -17,7 +17,20 @@
 # Copy the 64-bit primary, 32-bit secondary zygote startup script
 PRODUCT_COPY_FILES += \
 external/ubt/ubt_a32a64_bridge:system/vendor/lib64/libubt_a32a64_bridge.so \
-external/ubt/armv7-guest/system/bin/linker:system/vendor/armv7g-guest/system/bin/linker
+external/ubt/armv7-guest/data/.placeholder:system/vendor/armv7-guest/data/.placeholder \
+external/ubt/armv7-guest/mnt/.placeholder:system/vendor/armv7-guest/mnt/.placeholder \
+external/ubt/armv7-guest/proc/.placeholder:system/vendor/armv7-guest/proc/.placeholder \
+external/ubt/armv7-guest/sys/.placeholder:system/vendor/armv7-guest/sys/.placeholder \
+external/ubt/armv7-guest/dev/.placeholder:system/vendor/armv7-guest/dev/.placeholder \
+external/ubt/armv7-guest/mediator:system/vendor/armv7-guest/mediator \
+external/ubt/armv7-guest/system/bin/linker:system/vendor/armv7-guest/system/bin/linker \
+external/ubt/armv7-guest/system/lib/libc.so:system/vendor/armv7-guest/system/lib/libc.so \
+external/ubt/armv7-guest/system/lib/libc++.so:system/vendor/armv7-guest/system/lib/libc++.so \
+external/ubt/armv7-guest/system/lib/libdl.so:system/vendor/armv7-guest/system/lib/libdl.so \
+external/ubt/armv7-guest/system/lib/liblog.so:system/vendor/armv7-guest/system/lib/liblog.so \
+external/ubt/armv7-guest/system/lib/libm.so:system/vendor/armv7-guest/system/lib/libm.so \
+external/ubt/armv7-guest/system/lib/libstdc++.so:system/vendor/armv7-guest/system/lib/libstdc++.so \
+external/ubt/armv7-guest/vpaths-list:system/vendor/armv7-guest/vpaths-list
 
 # register ubt as native bridge 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
